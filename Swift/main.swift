fce83632f1e9a541773b529814e8f0db87c07c1f
KyubinLee <kyubin@kakao.com>
2022-06-30 01:15:11 +0900

    first commit

--- Swift/main.swift
+++ Swift/main.swift
@@ -7,5 +7,13 @@
 
 import Foundation
 
-print("Hello, World!")
+var variable = 0 // 변수 선언(타입추론)
+let constant : Int = 0 // 상수 선언(타입 어노테이션[지정])
+var integer : Int = 0 // 명시적인 Int 타입
+var string : String // 타입 어노테이션을 통해 명시적으로 데이터 타입을 지정한 경우 초기화를 동시에 하지 않아도 됩니다.
+var character : Character = "C" // 캐릭터는 문자 하나만을 저장할 수 있습니다. 하나 이상의 문자인 경우 문자열인 String형으로 선언해야 합니다.
+var double : Double = 23.6
+var float : Float
+var 💜 : Bool  = false // 스위프트는 이모지 또한 변수명으로 인식합니다.
 
+print("Hello Swift, \(variable), \(character), \(double)")
