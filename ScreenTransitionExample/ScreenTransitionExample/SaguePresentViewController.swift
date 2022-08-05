46029e0c7f0faa6c09f35ea2ac91ddd7ae978be2
KyubinLee <kyubin@kakao.com>
2022-08-05 14:18:50 +0900

    commit

---
+++ ScreenTransitionExample/ScreenTransitionExample/SaguePresentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SaguePresentViewController.swift
+//  ScreenTransitionExample
+//
+//  Created by Kyubin Lee on 2022/07/16.
+//
+
+import UIKit
+
+class SaguePresentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
