a383b53b4ac04a9f43d7a5d198ab490f67b09959
KyubinLee <kyubin@kakao.com>
2022-08-05 16:39:45 +0900

    commit

---
+++ Algorithm/조건문/9498.swift
@@ -0,0 +1,7 @@
+let score = Int(readLine()!)!
+
+if 100 >= score && score >= 90 { print("A") }
+else if 89 >= score && score >= 80 { print("B") }
+else if 79 >= score && score >= 70 { print("C") }
+else if 69 >= score && score >= 60 { print("D") }
+else { print("F") }     
